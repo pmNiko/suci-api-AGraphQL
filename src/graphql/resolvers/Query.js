fc30e5455597119fe43544fd9f6be97b9352ae23
pmNiko <nikolas090189@gmail.com>
2021-01-28 16:57:31 -0300

    Before change table

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -5,8 +5,8 @@ import Table from "../../models/Table";
 
 export const Query = {
   tasks: () => tasks,
-  dishes: async () => await Dish.find(),
-  orders: async () => await Order.find(),
-  order: async (_, { order_id }) => await Order.findById(order_id),
-  tables: async () => await Table.find(),
+  dishes: async () => await Dish.find().lean(),
+  orders: async () => await Order.find().lean(),
+  order: async (_, { order_id }) => await Order.findById(order_id).lean(),
+  tables: async () => await Table.find().lean(),
 };
